ad0d4e6e133816071924dadb4df622c2023bd4eb
Nvbess <Orellanaprdjulio@gmail.com>
2024-09-02 00:38:16 -0400

    TeLlevo_APP / Login / Register /

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
@@ -40,50 +39,6 @@ const routes: Routes = [
     path: 'inicio',
     loadChildren: () => import('./pages/inicio/inicio.module').then( m => m.InicioPageModule)
   },
-];
-=======
-import { NgModule } from '@angular/core';
-import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [
-  {
-    path: '',
-    redirectTo: 'splash-screen',
-    pathMatch: 'full'
-  },
-  {
-    path: 'folder/:id',
-    loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
-  },
-  {
-    path: 'splash-screen',
-    loadChildren: () => import('./pages/splash-screen/splash-screen.module').then( m => m.SplashScreenPageModule)
-  },
-  {
-    path: 'login',
-    loadChildren: () => import('./pages/registration/login/login.module').then( m => m.LoginPageModule)
-  },
-  {
-    path: 'register',
-    loadChildren: () => import('./pages/registration/register/register.module').then( m => m.RegisterPageModule)
-  },
-  {
-    path: 'pasajero-home',
-    loadChildren: () => import('./pages/pasajero/home/home.module').then( m => m.HomePageModule)
-  },
-  {
-    path: 'conductor-home',
-    loadChildren: () => import('./pages/conductor/home/home.module').then( m => m.HomePageModule)
-  },
-  {
-    path: 'admin-home',
-    loadChildren: () => import('./pages/admin/home/home.module').then( m => m.HomePageModule)
-  },  {
-    path: 'login-page',
-    loadChildren: () => import('./pages/registration/login-page/login-page.module').then( m => m.LoginPagePageModule)
-  }
->>>>>>> 9aa42d2797abfbacf5acd6989ed55e5f463d28b3
-
 ];
 
 @NgModule({
