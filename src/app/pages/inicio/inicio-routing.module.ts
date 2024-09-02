2b49975461a9cdb88ef6436b40a55eaf41dfdab3
Nvbess <Orellanaprdjulio@gmail.com>
2024-09-02 19:42:49 -0400

    Te Llevo APP / Inicio

--- src/app/pages/inicio/inicio-routing.module.ts
+++ src/app/pages/inicio/inicio-routing.module.ts
@@ -2,12 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { InicioPage } from './inicio.page';
+import { RegisterPage } from '../registration/register/register.page';
 
 const routes: Routes = [
-  {
-    path: '',
-    component: InicioPage
-  }
+  { path: '',component: InicioPage},
+  { path: 'register', component: RegisterPage },
 ];
 
 @NgModule({
--- src/app/pages/inicio/inicio.page.ts
+++ src/app/pages/inicio/inicio.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -7,6 +7,8 @@ import { Router } from '@angular/router';
   styleUrls: ['./inicio.page.scss'],
 })
 export class InicioPage implements OnInit {
+
+  @ViewChild('card', { static: false }) card!: ElementRef;
 
   constructor(private router: Router) { }
 
@@ -18,7 +20,12 @@ export class InicioPage implements OnInit {
     this.router.navigate(['login']);
   }
 
-  register() {
-    this.router.navigate(['register']);
+  ToRegister() {
+    if (this.card && this.card.nativeElement) {
+      this.card.nativeElement.classList.add('expand');
+    }
+    setTimeout(() => {
+      this.router.navigate(['/register']);
+    }, 300);
   }
 }
