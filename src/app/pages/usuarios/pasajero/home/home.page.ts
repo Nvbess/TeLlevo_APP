7412422fd24d0b53929ed0e296d971257ff55c74
Nvbess <Orellanaprdjulio@gmail.com>
2024-09-17 15:33:12 -0300

    TeLlevo_APP / Orden / Register / All

--- src/app/pages/usuarios/pasajero/home/home.page.ts
+++ src/app/pages/usuarios/pasajero/home/home.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { MenuController } from '@ionic/angular';
 import { ViajesService } from 'src/app/services/viajes.service';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-home',
@@ -16,10 +17,20 @@ export class HomePage implements OnInit {
   public apellUsuario?: string;
   public idUsuario?: number;
 
+  map: any;
+
   constructor(private router: Router,
               private menuController: MenuController,
               private viajesService: ViajesService) { }
 
+  ionViewDidEnter() {
+    this.map = L.map('mapId').setView([-33.598449986499055, -70.57880611157175], 17); 
+  L.tileLayer('https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.png', {
+    noWrap: true,
+  }).addTo(this.map);
+  this.map.zoomControl.setPosition('bottomright');
+  }
+             
   ngOnInit() {
     this.menuController.enable(true);
     const usuarioLogin = localStorage.getItem('usuarioLogin');
