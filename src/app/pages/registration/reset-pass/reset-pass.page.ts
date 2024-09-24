ef89d11cb597b3abadfe5a65c3ad53132ec8e7f4
Nvbess <Orellanaprdjulio@gmail.com>
2024-09-23 23:20:45 -0300

    TeLlevo_APP / Clase / Firebase

--- src/app/pages/registration/reset-pass/reset-pass.page.ts
+++ src/app/pages/registration/reset-pass/reset-pass.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from 'src/app/services/firebase/auth.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-reset-pass',
@@ -7,9 +10,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResetPassPage implements OnInit {
 
-  constructor() { }
+  recoveryForm: FormGroup;
 
-  ngOnInit() {
+  constructor(
+    private authService: AuthService, 
+    private formBuilder: FormBuilder
+  ) {
+    this.recoveryForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+    });
   }
 
+  ngOnInit() {}
+
+  async recoveryPass(email: string) {
+    try {
+      await this.authService.recoverypass(email);
+      Swal.fire({
+        icon: 'success',
+        title: 'Correo Enviado!',
+        confirmButtonText: 'OK',
+        heightAuto: false,
+      })
+    } catch (error) {
+      Swal.fire({
+        icon: 'error',
+        title: 'Error al enviar!',
+        confirmButtonText: 'OK',
+        heightAuto: false,
+      })
+    }
+  }
 }
+
